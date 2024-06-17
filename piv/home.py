35661a8a7497b4c7355aeff611894663ebd70d5f
celovieiras <celovieirads@gmail.com>
2024-06-17 00:24:09 -0300

    Removing the logo page in home.py

--- piv/home.py
+++ piv/home.py
@@ -3,8 +3,8 @@ import streamlit as st
 #config. aplicação streamlit
 st.set_page_config(page_title="Projeto Trepidação - PI-V", layout="wide")
 
-imagem_logo = "piv/images/fatec_logo.png"
-st.logo(image=imagem_logo, icon_image=imagem_logo)
+imagem_logo = "images/fatec_logo.png"
+# st.logo(image=imagem_logo, icon_image=imagem_logo)
 st.image(imagem_logo, output_format="PNG")
 st.title("Análise de trepidação em ambiente doméstico")
 st.text("Trabalho realizado por Marcelo Vieira da Silva e Thiago Ribeiro da Silva para Projeto Integrador V")
