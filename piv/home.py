8ed0e1d702dfe91498a4352f5fd42cfa641ee7ed
celovieiras <celovieirads@gmail.com>
2024-06-20 19:10:20 -0300

    Update pages e home.py_1

--- piv/home.py
+++ piv/home.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(page_title="Projeto Trepidação - PI-V", layout="wide")
 
 imagem_logo = "images/fatec_logo.png"
-st.logo(image=imagem_logo, icon_image=imagem_logo)
+st.logo(imagem_logo, icon_image=imagem_logo)
 st.image(imagem_logo, output_format="PNG")
 st.title("Análise de trepidação em ambiente doméstico")
 st.text("Trabalho realizado por Marcelo Vieira da Silva e Thiago Ribeiro da Silva para Projeto Integrador V")
--- piv/pages/sobre o grupo.py
+++ piv/pages/sobre o grupo.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.set_page_config(page_title='Grupo', layout='wide')
 imagem_logo = "images/fatec_logo.png"
-st.logo(image=imagem_logo, icon_image=imagem_logo)
+st.logo(imagem_logo, icon_image=imagem_logo)
 st.image(imagem_logo, output_format="PNG")
 st.title("Sobre o grupo")
