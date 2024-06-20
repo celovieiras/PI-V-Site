7ae81eebdac0bf852c6886eb31da53f85cd4e5aa
celovieiras <celovieirads@gmail.com>
2024-06-20 19:15:53 -0300

    Removing images code line

--- piv/pages/grafico.py
+++ piv/pages/grafico.py
@@ -26,9 +26,9 @@ data = load_dataframe()
 val_dt_padrao = dt.date(2024,5,30)
 
 st.set_page_config(page_title='Visualização da trepidação', layout='wide')
-imagem_logo = "images/fatec_logo.png"
-st.logo(imagem_logo, icon_image=imagem_logo)
-st.image(imagem_logo, output_format="PNG")
+# imagem_logo = "images/fatec_logo.png"
+# st.logo(imagem_logo, icon_image=imagem_logo)
+# st.image(imagem_logo, output_format="PNG")
 st.title("Análise de trepidação em ambiente doméstico")
 exclui_zero_trep = data['acelerometroZ'][data['acelerometroZ'] != 0]
 valor_min_trep = exclui_zero_trep.min()
