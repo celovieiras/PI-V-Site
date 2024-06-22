2ca432aee0dc0e54b23ae52f69dfde547f29e03a
celovieiras <celovieirads@gmail.com>
2024-06-22 09:55:12 -0300

    Removing pages: sobre o grupo, codigos

--- piv/pages/sobre o grupo.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title='Grupo', layout='wide')
-# imagem_logo = "images/fatec_logo.png"
-# st.logo(imagem_logo, icon_image=imagem_logo)
-# st.image(imagem_logo, output_format="PNG")
-st.title("Sobre o grupo")
