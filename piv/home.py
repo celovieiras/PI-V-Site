d8056e8fcda59fefe36e7515da970bff7e899786
celovieiras <celovieirads@gmail.com>
2024-06-22 10:13:15 -0300

    removing images & logo code line

--- piv/home.py
+++ piv/home.py
@@ -3,9 +3,6 @@ import streamlit as st
 #config. aplicação streamlit
 st.set_page_config(page_title="Projeto Trepidação - PI-V", layout="wide")
 
-# imagem_logo = "images/fatec_logo.png"
-# st.logo(imagem_logo, icon_image=imagem_logo)
-# st.image(imagem_logo, output_format="PNG")
 st.title("Análise de trepidação em ambiente doméstico")
 st.text("Trabalho realizado por Marcelo Vieira da Silva e Thiago Ribeiro da Silva para Projeto Integrador V")
 st.write("---")
