40c73df3ec764fc29a980dbed60bf4ad8e7097c6
celovieiras <celovieirads@gmail.com>
2024-06-25 00:55:23 -0300

    Adding another st.container()

--- piv/pages/grafico.py
+++ piv/pages/grafico.py
@@ -62,3 +62,14 @@ with st.container():
         fig = px.line(momentos_trepidacao.set_index('dt_cria')['acelerometroZ'], title='Registros de trepidação')
         # st.line_chart(momentos_trepidacao.set_index('dt_cria')['acelerometroZ'], color='#FEA82F', use_container_width=True)
         st.plotly_chart(fig, use_container_width=True)
+        
+    with st.container():
+        dt_hora_selec = dt.datetime.combine(dt_trep, hr_trep)
+        filtros_grafico = data[data['dt_cria'].isin(dt_hora_selec)]
+
+        filtros_grafico['diff'] = filtros_grafico['acelerometroZ'].diff().fillna(0).abs()
+        change_threshold = 0
+        momentos_trepidacao = filtros_grafico[(filtros_grafico['diff'] > change_threshold) & (filtros_grafico['acelerometroZ'] != 0)]
+        fig = px.line(momentos_trepidacao.set_index('dt_cria')['acelerometroZ'], title='Registros de trepidação')
+        # st.line_chart(momentos_trepidacao.set_index('dt_cria')['acelerometroZ'], color='#FEA82F', use_container_width=True)
+        st.plotly_chart(fig, use_container_width=True)
